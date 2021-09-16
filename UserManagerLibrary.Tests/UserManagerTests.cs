9eafec9e066dda71a5a7da002b76451bca65edcf
Karim Said <inkriment@gmail.com>
2021-09-16 16:28:23 +0500

    User manager library

---
+++ UserManagerLibrary.Tests/UserManagerTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UserManagerLibrary.Tests
+{
+    [TestClass]
+    public class UserManagerTests
+    {
+        public TestContext TestContext { get; set; }
+        private UserManager manager = new UserManager();
+
+
+        //DataSource - опредиляет источник данних
+        //1  параметр имя провайдера
+        //2  параметр строка подключения или пут к файлу
+        //3  параметр имя таблиса или или  элемент в XML
+        //4  параметр как присходит дуступ к записям из источник данних
+
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.XML",
+            "testData.xml",
+            "User",
+            DataAccessMethod.Sequential)]
+        [TestMethod]
+        public void UserManager_Add_FromXML()
+        {
+            string userId = Convert.ToString(TestContext.DataRow["userId"]);
+            string telephone = Convert.ToString(TestContext.DataRow["telephone"]);
+            string email = Convert.ToString(TestContext.DataRow["email"]);
+
+            bool result = manager.Add(userId, telephone, email);
+
+            Assert.IsTrue(result, "User can't  create");
+
+        }
+    }
+}
---
+++ UserManagerLibrary/UserManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace UserManagerLibrary
+{
+    public class UserManager
+    {
+        public bool Add(string userId,string phone,string email)
+        {
+            if(userId.Length<4)
+            {
+                throw new Exception("Your UserId must be more than 4 character ");
+            }
+            if(phone.Contains("a"))
+            {
+                throw new Exception("Your nuber phone must be characters");
+            }
+            if(!email.Contains("@"))
+            {
+                throw new Exception("An error in email name address");
+            }
+
+            //Statments for save on a DataBase or in a File
+            //....
+            //....
+
+            return true;
+        }
+    }
+}
